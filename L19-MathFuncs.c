964b862ea0815a82a7d60498f95deac57551b647
Jishnu Setia <jishnusetia8@gmail.com>
2026-01-29 00:28:56 +0400

    Adding L19

---
+++ L19-MathFuncs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+	// These are the available funcs you can get from math.h
+	printf("%f\n", sqrt(16));
+	printf("%f\n", ceil(1.4));
+	printf("%f\n", floor(1.4));
+	printf("%f\n", pow(4, 3));
+	// More funcs here - https://www.w3schools.com/c/c_ref_math.php
+
+	return 0;
+}
